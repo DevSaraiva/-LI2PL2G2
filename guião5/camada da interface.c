ce03b075bc58394d8991860a084ba385b273ff2c
Miguel Santa Cruz <a93194@alunos.uminho.pt>
2020-03-10 23:52:01 +0000

    Primeira versão funcional

--- guião5/camada da interface.c
+++ guião5/camada da interface.c
@@ -10,11 +10,12 @@ void mostrar_tabuleiro(ESTADO *estado) {
             if (estado->tab[i][j]) printf("*");
             else if (j == 7 && i==0) printf("2");
             else if (j==0 && i==7) printf("1");
+            else printf(".");
             for (int k = 0; k < 32 ; k++) {
                 if ((estado->jogadas[k].jogador1.linha == i && estado->jogadas[i].jogador1.coluna == j) || (estado->jogadas[k].jogador2.linha == i && estado->jogadas[k].jogador2.coluna == j))
                     printf("#");
-                else printf(".");
             }
+        
         }
         printf("\n");
     }
