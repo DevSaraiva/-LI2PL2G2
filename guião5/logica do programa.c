d50045887564c7615b6d09502c2aad9b4a68d168
Sofia Gomes <sofiargomes@hotmail.com>
2020-03-19 23:22:25 +0000

    resolvido conflito

--- guião5/logica do programa.c
+++ guião5/logica do programa.c
@@ -14,28 +14,77 @@ Módulo que contém toda a lógica do jogo
 \brief Função que altera o estado do jogo através das coordenadas fornecidas
 */
 
+/**
+\brief Função que verifica se uma jogada é possivel
+*/
 
-int jogar (ESTADO *estado, COORDENADA c){
-    
+int valida_jogada (ESTADO *estado,COORDENADA c){
+    int c_ult = estado -> ultima_jogada.coluna;
+    int l_ult = estado -> ultima_jogada.linha;
     int c_coluna = c.coluna;
     int c_linha = c.linha;
-    int jog = estado -> jogador_atual;
-
-        // Muda de jogador
-        if ( jog == 1) jog = 2;
-        else jog = 1;
-        
-
-        printf("jogar %d %d\n", c.coluna, c.linha);
-        estado -> tab [c.linha] [c.coluna] = BRANCA;
-        
-        // Aumenta número de jogadas
-        if (jog == 2) estado -> num_jogadas++;
-        
-       
-        
-        return 0;
-  
-
+    CASA x = VAZIO;
+    if (((c_coluna - c_ult)*(c_coluna - c_ult) <= 1) && ((l_ult - c_linha)*(l_ult - c_linha) <= 1) && (obter_estado_casa(estado,c)) == x) 
+        return 1;
+    else return 0;
 }
 
+
+/**
+\brief Função que se o jogo acabou
+*/
+
+
+int casa_vencedora (ESTADO *e,COORDENADA c){
+    int c_coluna = c.coluna;
+    int c_linha = c.linha;
+    int jog = e -> jogador_atual;
+    if ((jog == 1 && c_coluna == 1 && c_linha == 1) || (jog == 2 && c_coluna == 8 && c_linha == 8)) return 1;
+    else return 0;
+}
+
+/**
+\brief Função que altera o estado do jogo através das coordenadas fornecidas
+*/
+
+int jogar (ESTADO *estado, COORDENADA c){
+    int c_ult = estado -> ultima_jogada.coluna;
+    int l_ult = estado -> ultima_jogada.linha;
+    int c_coluna = c.coluna;
+    int c_linha = c.linha;
+    int j_atual = obter_jogador_atual (estado);
+    printf("jogar %d %d\n", c.coluna, c.linha);
+
+    if (valida_jogada(estado,c)==1){
+        // Atualiza o tabuleiro
+
+        estado -> tab [c.coluna -1] [c.linha-1] = BRANCA;
+        estado -> tab [c_ult-1] [l_ult-1] = PRETA;
+
+        // Muda de jogador e guarda a jogada
+        
+        if (j_atual == 1) estado -> jogador_atual = 2;
+        else {
+            estado -> jogador_atual = 1;
+            COORDENADA j1 = {c_ult,l_ult};
+            COORDENADA j2 = {c.coluna, c.linha};
+            JOGADA j = {j1,j2};
+            int i = 0;
+            estado -> jogadas [i] = j;
+            i++;
+        }
+
+        // Atualiza a última jogada
+        estado -> ultima_jogada.coluna = c_ult;
+        estado -> ultima_jogada.linha = l_ult;
+        
+    }    
+    else printf("Jogada inválida\n");
+
+    // Aumenta número de jogadas
+    if (j_atual == 2) 
+        estado -> num_jogadas++;
+
+    if (casa_vencedora (estado,c) == 1) printf("O vecedor é o jogador %d", j_atual-1);
+    
+}
