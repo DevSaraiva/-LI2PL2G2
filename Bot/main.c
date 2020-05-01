d0ee373925aa572ff00a83955b8feb86f05e711d
Miguel Santa Cruz <a93194@alunos.uminho.pt>
2020-05-01 15:33:45 +0100

    Bot mais funcional

--- Bot/main.c
+++ Bot/main.c
@@ -22,9 +22,19 @@ Aglomeração de todas as camadas
 
 
 int main(int argc, char *argv[]){
-    ESTADO* e = inicializar_estado();
-    ler_estado(e,argv[1]);
-    joga_aleatorio(e);
-    gravar_estado(e,argv[2]);
+    ESTADO *e = inicializar_estado();
+    char *filename1 = argv[1];
+    char *filename2 = argv[2];
+    mostrar_tabuleiro(e);
+    ler_estado(e,filename1);
+    COORDENADA coord = joga_euclidiana(e);
+    if (casa_vencedora (e,coord) || jogada_presa (e,coord)){
+                int j_atual = obter_jogador_atual (e);
+                printf("O vencedor é o PL%d\n",j_atual);
+                e -> num_jogadas = 32;}
+    gravar_estado (e,filename2);
+
+
+
     return 0;
 }
